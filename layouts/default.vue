24c8327ea198960d9a15a52c4a5ae56a4f82a394
Dennis W <contact@denniswillmann.com>
2020-07-02 16:27:58 +0200

    Initial Commit + Socket.io default config

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer absolute app>
+      <span>&copy; DeWil</span>
+    </v-footer>
+  </v-app>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-title>Work in Progress.</v-card-title>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
