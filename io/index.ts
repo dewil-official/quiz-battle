5d88abb303232a83b7bd66af8426e6d690c74a2f
Dennis W <contact@denniswillmann.com>
2020-07-07 18:34:13 +0200

    Updated /io/index.js to index.ts, Added @types/socket.io

--- io/index.ts
+++ io/index.ts
@@ -4,21 +4,22 @@ import AuthIO from './auth/authIO'
 import AuthUtils from './auth/authUtils'
 import PlayersIO from './players/playersIO'
 import GameIO from './game/gameIO'
+import { Module } from '@nuxt/types'
 
 let userFile = require('../data/users.json')
 
-export default function () {
+const ioModule: Module = function () {
   const authUtils = new AuthUtils(userFile)
   const authIO = new AuthIO(authUtils)
   const gameIO = new GameIO(authUtils)
   const playersIO = new PlayersIO()
 
-  this.nuxt.hook('render:before', (renderer) => {
+  this.nuxt.hook('render:before', () => {
     const server = http.createServer(this.nuxt.renderer.app)
     const io = socketIO(server)
 
     // overwrite nuxt.server.listen()
-    this.nuxt.server.listen = (port, host) =>
+    this.nuxt.server.listen = (port: any, host: any) =>
       new Promise((resolve) =>
         server.listen(port || 3000, host || 'localhost', resolve)
       )
@@ -26,10 +27,12 @@ export default function () {
     this.nuxt.hook('close', () => new Promise(server.close))
 
     // Add socket.io events
-    io.on('connection', (socket) => {
+    io.on('connection', (socket: WebSocket) => {
       authIO.registerSocketHandlers(socket)
       playersIO.registerSocketHandlers(socket)
       gameIO.registerSocketHandlers(socket)
     })
   })
 }
+
+export default ioModule
