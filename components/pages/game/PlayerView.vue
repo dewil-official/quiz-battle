769237a7c36de8fd9f0d7e87998afe47c6e5055e
Dennis W <contact@denniswillmann.com>
2020-08-18 11:26:56 +0200

    Refactor: Moved MasterView & PlayerView into game.vue page

--- components/pages/game/PlayerView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>Players View.</div>
-</template>
-
-<script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
-@Component
-export default class PlayerView extends Vue {}
-</script>
--- pages/game.vue
+++ pages/game.vue
@@ -1,33 +1,63 @@
 <template>
   <div>
-    <MasterView v-if="hasGameData" :gameData="gameState.gameData" />
-    <LoadingPage v-if="!hasGameData" />
+    <div v-if="hasGameData">
+      <Waiting v-if="gameStage == GameStage.waiting" />
+      <Intro v-if="gameStage == GameStage.intro" />
+      <Question v-if="gameStage == GameStage.question" :gameData="gameData" />
+      <AnswerEvaluation v-if="gameStage == GameStage.answerEvaluation" :gameData="gameData" />
+      <QuestionResults v-if="gameStage == GameStage.questionResults" :gameData="gameData" />
+      <Paused v-if="gameStage == GameStage.paused" />
+      <End v-if="gameStage == GameStage.end" />
+    </div>
+    <LoadingPage v-else />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue, State } from 'nuxt-property-decorator'
+import AuthState from '~/types/interfaces/auth/authState'
+import GameState from '~/types/interfaces/game/gameState'
+import { GameData } from '~/types/interfaces/game/gameUpdate'
+import { GameStage } from '~/types/enums/game/gameStage'
+import AuthStore from '~/store/auth'
 import MasterView from '~/components/pages/game/MasterView.vue'
 import PlayerView from '~/components/pages/game/PlayerView.vue'
 import LoadingPage from '~/components/shared/LoadingPage.vue'
-import { GameData } from '../types/interfaces/game/gameUpdate'
-import AuthStore from '../store/auth'
-import AuthState from '../types/interfaces/auth/authState'
-import GameState from '../types/interfaces/game/gameState'
+import Waiting from '~/components/pages/game/master/Waiting.vue'
+import Intro from '~/components/pages/game/master/Intro.vue'
+import Question from '~/components/pages/game/master/Question.vue'
+import AnswerEvaluation from '~/components/pages/game/master/AnswerEvaluation.vue'
+import QuestionResults from '~/components/pages/game/master/QuestionResults.vue'
+import Paused from '~/components/pages/game/master/Paused.vue'
+import End from '~/components/pages/game/master/End.vue'
 
 @Component({
   components: {
     MasterView,
     PlayerView,
     LoadingPage,
+    Waiting,
+    Intro,
+    Question,
+    AnswerEvaluation,
+    QuestionResults,
+    Paused,
+    End,
   },
 })
 export default class GamePage extends Vue {
+  // Makes type available inside <template>
+  GameStage: any = GameStage
+
   @State('game') gameState!: GameState
   @State('auth') authState!: AuthState
 
   get gameData(): GameData | null {
     return this.gameState.gameData
+  }
+
+  get gameStage(): GameStage | null {
+    return this.gameData?.gameInfo.gameStage ?? null
   }
 
   get hasGameData(): boolean {
